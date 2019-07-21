a5777895d4f864a93634ef7171026a6252e6775c
David Troidl <DavidTroidl@aol.com>
2019-07-21 14:11:26 -0400

    Initial release of OSHB Read

---
+++ read/Script/AccentInterpretation.js
@@ -0,0 +1,40 @@
+/**
+ * @fileOverview AccentInterpretation is a component for OshbRead,
+ * to interpret cantillation marks.
+ * @version 1.0
+ * @author David
+ */
+accentInterpretation = function() {
+	var accentCatalog = window.accentCatalog,
+		accents = accentCatalog.prose;
+	// Set the scope.
+	function setScope(osisID) {
+		var fields = osisID.split(".");
+		switch (fields[0]) {
+			case "Ps":
+			case "Prov":
+				return accentCatalog.poetic;
+			case "Job":
+				if (fields[1] < 3 || (fields[1] == 42 && fields[2] > 6) || osisID == "Job.3.1") {
+					return accentCatalog.prose;
+				} else {
+					return accentCatalog.poetic;
+				}
+				break;
+			default:
+				return accentCatalog.prose;
+		}
+	}
+	// Interprets the accents.
+	function interpretAccents(accentString, accentType) {
+		return accents[accentType][accentString] ? accents[accentType][accentString].name : "error|" + accentString + "|" + accentType;
+	}
+	return {
+		setAccents: function(osisID) {
+			accents = setScope(osisID);
+		},
+		interpret: function(accentString, accentType) {
+			return interpretAccents(accentString, accentType);
+		}
+	};
+}();
---
+++ read/Script/ChapterMarkup.js
@@ -0,0 +1,33 @@
+/**
+ * @fileOverview ChapterMarkup is a markup component for OshbRead.
+ * @version 1.0
+ * @author David
+ */
+chapterMarkup = function() {
+	// Convert an osisID to a scripture reference.
+	var refConvert = window.referenceConversion;
+	// Marks up the sequence of verses.
+	function verseElements(verses) {
+		var para = document.createElement('p'),
+			i = 0, lim = verses.length;
+		for (; i < lim; i++) {
+			// Send verses to ElementMarkup.
+			para.appendChild(elementMarkup(verses[i]));
+		}
+		return para;
+	}
+    // Marks up the chapter.
+	function chapterElement(chapter) {
+		var verses = chapter.getElementsByTagName('verse'),
+			sec = document.createElement('section'),
+			heading = document.createElement('h3');
+		heading.appendChild(document.createTextNode(refConvert(chapter.getAttribute('osisID'))));
+		sec.appendChild(heading);
+		// Append verses.
+		sec.appendChild(verseElements(verses));
+		return sec;
+	}
+	return function(chapterXML) {
+		return chapterElement(chapterXML);
+	};
+}();
